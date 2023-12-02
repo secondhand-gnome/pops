42d09ceab00aca93b8c4aa8c0cb11fd0d323e866
Austin Almond <aust.almond@gmail.com>
2023-12-02 15:24:02 -0500

    Add zip to wasm script

--- scripts/generate-wasm.sh
+++ scripts/generate-wasm.sh
@@ -1,3 +1,6 @@
 # see https://bevy-cheatbook.github.io/platforms/wasm/webpage.html
 cargo build --release --target wasm32-unknown-unknown
 wasm-bindgen --no-typescript --target web --out-dir ./out/ --out-name "pops" ./target/wasm32-unknown-unknown/release/pops.wasm
+
+cd out
+tar -a -c -f pops.zip *.html *.wasm *.js
