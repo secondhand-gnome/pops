42401583a00c63363f44e83b96d8aff8352a28bc
Austin Almond <aust.almond@gmail.com>
2023-12-02 14:51:33 -0500

    Render a boring square
    See https://bevyengine.org/examples/2D%20Rendering/2d-shapes/

--- src/main_game/mod.rs
+++ src/main_game/mod.rs
@@ -1,9 +1,30 @@
 use bevy::prelude::*;
+use bevy::sprite::MaterialMesh2dBundle;
 
 pub struct MainGamePlugin;
 
 impl Plugin for MainGamePlugin {
     fn build(&self, app: &mut App) {
-        // TODO
+        app.add_systems(Startup, spawn_camera)
+            .add_systems(Startup, render_a_boring_square);
     }
+}
+
+fn render_a_boring_square(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    commands.spawn(MaterialMesh2dBundle {
+        mesh: meshes
+            .add(shape::Quad::new(Vec2::new(50., 100.)).into())
+            .into(),
+        material: materials.add(ColorMaterial::from(Color::LIME_GREEN)),
+        transform: Transform::from_translation(Vec3::new(50., 0., 0.)),
+        ..default()
+    });
+}
+
+fn spawn_camera(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
 }
