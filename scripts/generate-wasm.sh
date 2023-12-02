513aa45c248d5e5f29ea998f9038482aca8454d5
Austin Almond <aust.almond@gmail.com>
2023-12-02 15:25:36 -0500

    return to previous dir after making zip

--- scripts/generate-wasm.sh
+++ scripts/generate-wasm.sh
@@ -2,5 +2,7 @@
 cargo build --release --target wasm32-unknown-unknown
 wasm-bindgen --no-typescript --target web --out-dir ./out/ --out-name "pops" ./target/wasm32-unknown-unknown/release/pops.wasm
 
+# Generate a ZIP of the web files
 cd out
 tar -a -c -f pops.zip *.html *.wasm *.js
+cd -
