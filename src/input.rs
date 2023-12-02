08bca4c7833b38edddefe501309dd9385a0d0585
Austin Almond <aust.almond@gmail.com>
2023-12-02 18:07:41 -0500

    Send click events

--- src/input.rs
+++ src/input.rs
@@ -18,28 +18,43 @@ pub struct Mouse {
 
 impl Plugin for InputPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(PreUpdate, update_mouse_pos)
+        app.add_systems(PreUpdate, update_mouse)
             .insert_resource(Mouse { pos: None })
             .add_event::<ClickEvent>();
     }
 }
 
-fn update_mouse_pos(
+fn update_mouse(
     mut mouse: ResMut<Mouse>,
+    mut mouse_buttons: Res<Input<MouseButton>>,
+    mut ev_click: EventWriter<ClickEvent>,
+    // TODO support touch input
     q_window: Query<&Window>,
     q_camera: Query<(&Camera, &GlobalTransform), With<MainCamera>>,
 ) {
     let window = q_window.single();
     let (camera, camera_transform) = q_camera.single();
 
-    if let Some(cursor_pos) = window.cursor_position() {
-        if let Some(ray) = camera.viewport_to_world(camera_transform, cursor_pos) {
-            let world_pos = ray.origin.truncate();
+    if let Some(world_pos) = window
+        .cursor_position()
+        .and_then(|cursor_pos| camera.viewport_to_world(camera_transform, cursor_pos))
+        .map(|ray| ray.origin.truncate())
+    {
+        // println!("MousePos is {:?}", world_pos);
+        mouse.pos = Some(world_pos);
+    } else {
+        mouse.pos = None;
+    }
 
-            // println!("MousePos is {:?}", world_pos);
-            mouse.pos = Some(world_pos);
-            return;
+    if mouse_buttons.just_pressed(MouseButton::Left) {
+        match mouse.pos {
+            Some(pos) => {
+                info!("Click! at {:?}", pos);
+                ev_click.send(ClickEvent { pos });
+            }
+            None => {
+                warn!("Somehow clicked with no mouse position!");
+            }
         }
     }
-    mouse.pos = None;
 }
