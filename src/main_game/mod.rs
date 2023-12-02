4aa02dcc3c8b0a807d1e3d4182a61e07c520664c
Austin Almond <aust.almond@gmail.com>
2023-12-02 17:19:31 -0500

    no more boring square

--- src/main_game/mod.rs
+++ src/main_game/mod.rs
@@ -1,7 +1,6 @@
 mod kernel;
 
 use bevy::prelude::*;
-use bevy::sprite::MaterialMesh2dBundle;
 
 use crate::config::hex;
 
@@ -11,30 +10,13 @@ pub struct MainGamePlugin;
 
 impl Plugin for MainGamePlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugins(KernelPlugin).add_systems(
-            Startup,
-            (spawn_camera, render_background, render_a_boring_square),
-        );
+        app.add_plugins(KernelPlugin)
+            .add_systems(Startup, (spawn_camera, render_background));
     }
 }
 
 fn render_background(mut commands: Commands) {
     commands.insert_resource(ClearColor(hex("#090a14")));
-}
-
-fn render_a_boring_square(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<ColorMaterial>>,
-) {
-    commands.spawn(MaterialMesh2dBundle {
-        mesh: meshes
-            .add(shape::Quad::new(Vec2::new(50., 100.)).into())
-            .into(),
-        material: materials.add(ColorMaterial::from(Color::LIME_GREEN)),
-        transform: Transform::from_translation(Vec3::new(0., 0., 0.)),
-        ..default()
-    });
 }
 
 fn spawn_camera(mut commands: Commands) {
