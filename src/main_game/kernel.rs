c9960ed637b17359f3184b03e824a5f7e6ec0ccf
Austin Almond <aust.almond@gmail.com>
2023-12-02 17:19:02 -0500

    render an empty kernel entity

--- src/main_game/kernel.rs
+++ src/main_game/kernel.rs
@@ -4,6 +4,30 @@ pub struct KernelPlugin;
 
 impl Plugin for KernelPlugin {
     fn build(&self, app: &mut App) {
-        // TODO
+        app.add_systems(Startup, spawn_first_kernel)
+            .register_type::<Kernel>()
+            .register_type::<KernelState>();
     }
+}
+
+#[derive(Debug, Default, Reflect)]
+enum KernelState {
+    #[default]
+    Raw,
+    Popped,
+}
+
+#[derive(Component, Debug, Default, Reflect)]
+struct Kernel {
+    state: KernelState,
+}
+
+#[derive(Bundle, Debug, Default)]
+struct KernelBundle {
+    kernel: Kernel,
+    // TODO sprite and transform
+}
+
+fn spawn_first_kernel(mut commands: Commands) {
+    commands.spawn((KernelBundle { ..default() }, Name::new("Kernel")));
 }
