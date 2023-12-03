a43eb0d8558f57e2ca1a665c587cdec3f664baac
Austin Almond <aust.almond@gmail.com>
2023-12-02 22:13:51 -0500

    add empty UI plugin

--- src/main_game/mod.rs
+++ src/main_game/mod.rs
@@ -1,15 +1,16 @@
 mod kernel;
 mod layers;
 mod skillet;
+mod ui;
 
 use bevy::prelude::*;
 
-use self::{kernel::KernelPlugin, skillet::SkilletPlugin};
+use self::{kernel::KernelPlugin, skillet::SkilletPlugin, ui::UiPlugin};
 
 pub struct MainGamePlugin;
 
 impl Plugin for MainGamePlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugins((KernelPlugin, SkilletPlugin));
+        app.add_plugins((KernelPlugin, SkilletPlugin, UiPlugin));
     }
 }
