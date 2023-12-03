45107a5db7c8319b766fa0244a2398e1477bf93b
Austin Almond <aust.almond@gmail.com>
2023-12-03 16:40:27 -0500

    update wasm script to include assets

--- scripts/generate-wasm.sh
+++ scripts/generate-wasm.sh
@@ -2,7 +2,11 @@
 cargo build --release --target wasm32-unknown-unknown
 wasm-bindgen --no-typescript --target web --out-dir ./out/ --out-name "pops" ./target/wasm32-unknown-unknown/release/pops.wasm
 
+# Copy the assets over
+rm -rf out/assets
+cp -r assets out/assets
+
 # Generate a ZIP of the web files
 cd out
-tar -a -c -f pops.zip *.html *.wasm *.js
+tar -a -c -f pops.zip *.html *.wasm *.js assets
 cd -
