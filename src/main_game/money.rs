9f78a2f239de8f9a62dd782fb5888c400a62b6bd
Austin Almond <aust.almond@gmail.com>
2023-12-03 15:44:39 -0500

    Implement bank account

--- src/main_game/money.rs
+++ src/main_game/money.rs
@@ -1,9 +1,65 @@
 use bevy::prelude::*;
+use bigdecimal::{
+    num_bigint::{BigInt, ToBigInt},
+    BigDecimal, FromPrimitive, ToPrimitive,
+};
+use num_format::{Locale, ToFormattedString};
+use std::{fmt, ops::Mul};
 
 pub struct MoneyPlugin;
 
 impl Plugin for MoneyPlugin {
     fn build(&self, app: &mut App) {
-        //
+        app.insert_resource(BankAccount::default())
+            .add_systems(Update, print_balance);
     }
+}
+
+#[derive(Resource, Default)]
+pub struct BankAccount {
+    balance: BigDecimal,
+}
+
+impl BankAccount {
+    pub fn credit(&mut self, amount: f32) {
+        self.balance += BigDecimal::from_f32(amount).unwrap().round(2);
+    }
+
+    pub fn debit(&mut self, amount: f32) {
+        self.balance -= BigDecimal::from_f32(amount).unwrap().round(2);
+    }
+
+    pub fn has_at_least(&self, amount: f32) -> bool {
+        self.balance >= BigDecimal::from_f32(amount).unwrap()
+    }
+
+    /// Return the dollars part of the balance.
+    fn whole_dollars(&self) -> BigInt {
+        self.balance.to_bigint().unwrap()
+    }
+
+    /// Return the cents part of the balance.
+    fn cents(&self) -> u8 {
+        (self.balance.clone() % BigDecimal::from(1))
+            .mul(BigDecimal::from(100))
+            .to_u8()
+            .unwrap()
+    }
+}
+
+impl fmt::Display for BankAccount {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "${}.{}",
+            self.whole_dollars().to_formatted_string(&Locale::en),
+            self.cents()
+        )
+    }
+}
+
+fn print_balance(mut acct: ResMut<BankAccount>) {
+    // Gain a nickel every frame
+    acct.credit(1000.05);
+    println!("{}", acct.as_ref());
 }
