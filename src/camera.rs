a8541ab807919ba478bbf9bcdccf19583f9215b1
Austin Almond <aust.almond@gmail.com>
2023-12-04 08:57:24 -0500

    add zoom out event

--- src/camera.rs
+++ src/camera.rs
@@ -7,9 +7,14 @@ pub struct CameraPlugin;
 #[derive(Component)]
 pub struct MainCamera;
 
+#[derive(Event)]
+pub struct ZoomOutEvent;
+
 impl Plugin for CameraPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(PreStartup, (spawn_camera, render_background));
+        app.add_systems(PreStartup, (spawn_camera, render_background))
+            .add_systems(Update, zoom_out_listener)
+            .add_event::<ZoomOutEvent>();
     }
 }
 
@@ -19,4 +24,14 @@ fn spawn_camera(mut commands: Commands) {
 
 fn render_background(mut commands: Commands) {
     commands.insert_resource(ClearColor(hex(BACKGROUND_COLOR)));
+}
+
+fn zoom_out_listener(
+    mut ev_zoom_out: EventReader<ZoomOutEvent>,
+    mut q_camera: Query<&mut Transform, With<MainCamera>>,
+) {
+    for _ in ev_zoom_out.read() {
+        let mut camera_transform = q_camera.single_mut();
+        camera_transform.scale *= 2.;
+    }
 }
