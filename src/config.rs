d2973f8b60edb9590f1af4ffa83863fc0f79548d
Austin Almond <aust.almond@gmail.com>
2023-12-06 22:23:01 -0500

    disable cheats

--- src/config.rs
+++ src/config.rs
@@ -1,6 +1,6 @@
 use bevy::prelude::*;
 
-pub const ENABLE_CHEATS: bool = true;
+pub const ENABLE_CHEATS: bool = false;
 pub const ENABLE_AUTO_KETTLE: bool = false;
 
 pub const BACKGROUND_COLOR: &str = "#3c5e8b";
